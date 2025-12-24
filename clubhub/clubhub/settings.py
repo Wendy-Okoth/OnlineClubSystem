657aabd9942b38f006547fbc977df4ac06baab4a
Wendy-Okoth <wendyaokoth@gmail.com>
2025-12-24 11:40:00 +0300

    updated settings.py

--- clubhub/clubhub/settings.py
+++ clubhub/clubhub/settings.py
@@ -79,6 +79,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (optional, for uploads)
 MEDIA_URL = '/media/'
